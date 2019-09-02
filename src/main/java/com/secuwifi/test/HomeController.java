81d0c0a5fd16cc1ba773b1ff1e0aaf2df53e835e
soyoon <tremexplo8@daum.net>
2019-09-03 03:22:44 +0900

    initial commit

---
+++ src/main/java/com/secuwifi/test/HomeController.java
@@ -0,0 +1,42 @@
+package com.secuwifi.test;
+
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	HomeDao homeDao;
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Model model) {
+
+		List<HomeDto> list = homeDao.sel();
+
+		for(int i=0; i<list.size(); i++){
+			logger.info(list.get(i).getFld_id());
+			logger.info(list.get(i).getFld_name());
+			model.addAttribute("ID", list.get(0).getFld_name() );
+
+
+		}
+
+		return "home";
+
+	}
+
+}
---
+++ src/main/java/com/secuwifi/test/HomeDto.java
@@ -0,0 +1,24 @@
+package com.secuwifi.test;
+
+public class HomeDto {
+
+	private String fld_id;
+	private String fld_name;
+	public String getFld_id() {
+		return fld_id;
+	}
+	public void setFld_id(String fld_id) {
+		this.fld_id = fld_id;
+	}
+	public String getFld_name() {
+		return fld_name;
+	}
+	public void setFld_name(String fld_name) {
+		this.fld_name = fld_name;
+	}
+	
+	
+
+
+
+}
